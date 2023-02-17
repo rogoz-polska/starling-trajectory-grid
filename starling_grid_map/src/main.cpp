357c7d4a52efd146ea51cc2062eac886849bd082
iman01 <iman.esfandiyar71@gmail.com>
2023-02-17 20:45:39 +0100

    launch added, multi file

---
+++ starling_grid_map/src/main.cpp
@@ -0,0 +1,12 @@
+#include "rclcpp/rclcpp.hpp"
+
+#include "starling_grid_map/starling_grid.hpp"
+
+int main(int argc, char ** argv)
+{
+  rclcpp::init(argc, argv);
+  RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Starling Grid map generator started");
+  rclcpp::spin(std::make_shared<starling_grid_map::AgrorobStatePublisher>());
+  rclcpp::shutdown();
+  return 0;
+}
